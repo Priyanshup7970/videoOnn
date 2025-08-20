53acd0c523eeeae5134ada4b123b988efd03f5c4
Priyanshup7970 <priyanshup7970@gmail.com>
2025-08-20 12:45:15 +0530

    utils

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,11 @@
-const asyncHandler = () => {}
+//promises method
+ 
+const asyncHandler = (requestHandler) => {
+    (req,res,next) =>{
+        promise.resolve(requestHandler(req,res,next)).
+        catch((err) => next(err))
+    }
+}
 
 
 
@@ -11,4 +18,18 @@ export { asyncHandler }
 /*
 const asynchandler = (func) => {}
 const asyncHandler = (func) => () => {}
+const asyncHandler = (func) => async() => {}
+*/
+/* try catch method
+const asyncHandler = (fn) => async(req,res,next) =>{
+    try{
+        await fn(req,res,next)
+    }catch(error){
+        res,status(error.code ||500).json({
+            successs: false,
+            message: error.message
+        })
+    }
+}
+
 */
