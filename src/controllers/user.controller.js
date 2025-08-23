74908eb54f03da000e1b8c1309849528f8fa4958
Priyanshup7970 <priyanshup7970@gmail.com>
2025-08-23 12:17:47 +0530

    register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const registerUser = asyncHandler(async(req,res) =>{
@@ -35,8 +37,40 @@ const registerUser = asyncHandler(async(req,res) =>{
         throw new ApiError(409, "User with email or username already exist ")
     }
 
-    req.files?.avatar
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    console.log("req.files =>", req.files);
+    console.log("avatarLocalPath =>", avatarLocalPath);
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    if(avatarLocalPath){
+        throw new ApiError(400,"avatar is required")
+    }
 
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar ){
+         throw new ApiError(400,"avatar is required")
+    }
+
+    const user = awaitUser.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage:coverImage?.url ||"", //return empty if no cover image otherwise retunr cover image url
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken" // this .select remove password and refreshToken to bre shown
+    )
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong while registering the user")
+    }
+
+    return res.staus(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 
 }) 
 
